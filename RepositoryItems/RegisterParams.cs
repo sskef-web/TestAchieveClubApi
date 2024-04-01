773255f42a9abeb7e718bc95889fbdd3b232be2b
Vlad Fedorovich <72762763+sskef-web@users.noreply.github.com>
2024-04-01 20:13:58 +0300

    fsdfs

---
+++ RepositoryItems/RegisterParams.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace TestAchieveClubApi.RepositoryItems
+{
+    public class RegisterParams
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Avatar { get; set; }
+        public int ClubRefId { get; set; }
+
+        public User ToUser()
+        {
+            return new User
+            {
+                Id = 0,
+                FirstName = FirstName,
+                LastName = LastName,
+                Email = Email,
+                Avatar = Avatar,
+                ClubRefId = ClubRefId,
+                Password = Password
+            };
+        }
+    }
+}
